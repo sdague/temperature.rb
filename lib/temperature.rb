e14ae78725914bf2857e206a360688e703bb51bc
Sean Dague <sean@dague.net>
2010-03-05 18:57:21 -0500

    added in documentation and license

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,10 +1,30 @@
+# Temperature works as a mixin on both the Numeric and String classes.
+# The idea is to make manipulating temperatures as simple and natural
+# as possible.  If units are not specified, they are assume to be in
+# degrees 'F'.  Yes, I realize this is US centric, but that's where I
+# live.  In the future I'd like to auto detect that base on locale.
+#
+# Example use:
+#
+#   freezing_in_C = 32.to_C
+#
+#   num = 0
+#   num.units = "C"
+#   freezing_in_F = num.to_F
+
 class Numeric
     @units
-    
+
+    # The scale factor between C and F
     CScale = 1.8
+    # The offset between C and F
     FOffset = 32
+    # The offset between K and C
     KOffset = 273.15
 
+    # The degree units the temperature is in.  This defaults to "F" if
+    # not specified.  Valid values are "C", "F", or "K" (R is not
+    # currently support... no one really uses that anyway).
     def units
         if @units
             return @units
@@ -21,18 +41,23 @@ class Numeric
         return @units
     end
     
+    # Is this a Farenheit temperature, returns a boolean
     def is_F?
         return self.units == "F"
     end
 
+    # Is this a Celcius temperature, returns a boolean
     def is_C?
         return self.units == "C"
     end
 
+    # Is this a Kelvin temperature, returns a boolean
     def is_K?
         return self.units == "K"
     end
 
+    # Convert the temperature to Farenheit.  If it's already in F, it
+    # returns itself.
     def to_F
         case self.units
         when "F":
@@ -44,6 +69,8 @@ class Numeric
         end
     end
 
+    # Convert the temperature to Celcius.  If it's already in C, it
+    # returns itself.
     def to_C
         case self.units
         when "F":
@@ -55,6 +82,8 @@ class Numeric
         end
     end
         
+    # Convert the temperature to Kelvins.  If it's already in K, it
+    # returns itself.
     def to_K
         case self.units
         when "F":
@@ -97,7 +126,11 @@ class Numeric
     def k2f
         self.k2c.c2f
     end
-    
+
+    # Compute the dewpoint for the temperature given a relative
+    # humidity.  This is using the NOAA approximation for dewpoint
+    # calculation -
+    # http://en.wikipedia.org/wiki/Dew_point#Closer_approximation
     def dewpoint(rh)
         units = self.units
         temp = self.to_C
@@ -113,6 +146,17 @@ class Numeric
 end
 
 class String
+    
+    # Parse a string that looks like a temperature into a temperature
+    # of the right units.  It handles strings of the format
+    #   
+    #   35.1C
+    #   72K
+    #   -40F
+    #
+    # Decimals are support, as are negative numbers.  The units must
+    # be C, K, or F, and not have a space between the numer and the
+    # units
     def to_degrees
         if self =~ /^(-?)(\d+)\.(\d+)(F|C|K)$/
             tmp = "#{$1}#{$2}.#{$3}".to_f
