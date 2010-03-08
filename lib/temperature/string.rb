a7ea3f3e325f7e7e37f5a66c403b6252c55c592c
Sean Dague <sean@dague.net>
2010-03-07 21:35:31 -0500

    move to seperate files

---
+++ lib/temperature/string.rb
@@ -0,0 +1,25 @@
+class String
+    
+    # Parse a string that looks like a temperature into a temperature
+    # of the right units.  It handles strings of the format
+    #   
+    #   35.1C
+    #   72K
+    #   -40F
+    #
+    # Decimals are support, as are negative numbers.  The units must
+    # be C, K, or F, and not have a space between the numer and the
+    # units
+    def to_degrees
+        if self =~ /^(-?)(\d+)\.(\d+)(F|C|K)$/
+            tmp = "#{$1}#{$2}.#{$3}".to_f
+            tmp.units = $4
+            return tmp
+        elsif self =~ /^(-?)(\d+)(F|C|K)$/
+            tmp = "#{$1}#{$2}".to_f
+            tmp.units = $3
+            return tmp
+        end
+    end
+end
+
