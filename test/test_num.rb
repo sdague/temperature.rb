b49fd16c90850a82f49e38622b792014d9306691
Matt Nelson <mnelson@advancedprosolutions.com>
2010-10-27 22:05:11 +0800

    Adding assertions for Rankine

--- test/test_num.rb
+++ test/test_num.rb
@@ -17,12 +17,14 @@ class NumTest < Test::Unit::TestCase
         f = [32.0, 212.0, -40.0, 32, 212, -40]
         c = [0.0, 100.0, -40.0, 0, 100, -40]
         k = [273.15, 373.15, 233.15, 273.15, 373.15, 233.15]
+        r = [491.67, 671.67, 419.67, 491.67, 671.67, 419.67]
         
         f.each_with_index do |n, i|
             temp = n
             assert_in_delta(temp.to_C, c[i], 0.00001)
             assert_in_delta(temp.to_F, f[i], 0.00001)
             assert_in_delta(temp.to_K, k[i], 0.00001)
+            assert_in_delta(temp.to_R, r[i], 0.00001)
         end
     end
 end
